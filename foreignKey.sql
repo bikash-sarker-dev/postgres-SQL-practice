729a8367c9dfeb575e6a6ac47d099b463d266dc2
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-20 10:50:14 +0600

    the join table

--- foreignKey.sql
+++ foreignKey.sql
@@ -31,3 +31,20 @@ INSERT INTO post (title, user_id) VALUES
     ('this is a tapos', 3),
     ('this is a romon', 5);
 SELECT * FROM post;
+
+DELETE FROM user3
+    WHERE id = 3;
+
+
+SELECT title, username from post
+    JOIN user3 ON post.user_id = user3.id;;
+
+SELECT user3.id from post 
+    JOIN user3 ON post.user_id = user3.id;
+
+SELECT * from post p
+    JOIN user3 u ON p.user_id = u.id;
+
+
+SELECT * from post as p
+    JOIN user3 as u ON p.user_id = u.id;
